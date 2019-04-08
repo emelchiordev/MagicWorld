5e189b94d84489d390d1256a474b1146757199e3
odyssee974 <manu.melchior@gmail.com>
2019-04-08 11:18:11 +0200

    Ajout d'une classe abstraite "personnages" qui sera parent des classes guerriers, mages et rodeurs.

---
+++ src/Personnages.java
@@ -0,0 +1,55 @@
+public abstract class Personnages {
+    protected int level;
+    protected int life;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+
+    public Personnages(int level, int life, int strength, int agility, int intelligence) {
+        this.level = level;
+        this.life = life;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+}
