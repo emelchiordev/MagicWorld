03b1562cdbee5e7186eea52f25ca821713c96547
odyssee974 <manu.melchior@gmail.com>
2019-04-08 11:25:16 +0200

    Ajout de 3 classes filles qui héritent de Personnage

---
+++ src/Mage.java
@@ -0,0 +1,5 @@
+public class Mage extends Personnages {
+    public Mage(int level, int life, int strength, int agility, int intelligence) {
+        super(level, life, strength, agility, intelligence);
+    }
+}
---
+++ src/Rodeur.java
@@ -0,0 +1,5 @@
+public class Rodeur extends Personnages{
+    public Rodeur(int level, int life, int strength, int agility, int intelligence) {
+        super(level, life, strength, agility, intelligence);
+    }
+}
