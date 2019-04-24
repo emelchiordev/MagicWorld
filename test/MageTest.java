0a8864169e730cbc4a51e5b255f5d84c6e4fe928
odyssee974 <manu.melchior@gmail.com>
2019-04-24 16:51:37 +0200

    Ajout d'un deuxième tests sur les Mages qui permet de tester le scenario où le soin ne doit pas dépasser la vie du personnage

--- test/MageTest.java
+++ test/MageTest.java
@@ -25,6 +25,13 @@ class MageTest {
         persoPlayer1.specialAttack(player1,player2);
         assertEquals(6,player1.getLifePlayer());
 
+    }
+    @Test
+    void When_PersoPlayer1UseSpecialAttackOnPlayer2_Then_Player11RecoveryLife_AndLimitRecovery() {
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
+        Mage persoPlayer1 = new Mage(10,0,5,3,2);
+
         player1.setLifePlayer(49);
         persoPlayer1.specialAttack(player1,player2);
         assertEquals(50,player1.getLifePlayer());
