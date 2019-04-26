6c1b0520a59d31db754a14850b8cd444c0994c56
odyssee974 <manu.melchior@gmail.com>
2019-04-26 12:12:31 +0200

    Ajout de test unitaire sur les scénarios suivants : - Quand le niveau du Gurrier est 10 alors la vie du joueur doit être 50 - Si le total des attributs du personnage dépasse le niveau du joueur, lève une exception.

--- test/PersonnagesMakerTest.java
+++ test/PersonnagesMakerTest.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonnagesMakerTest {
 
-    @Test
-    void When_LevelCharacterPlayer1IsEntered_Then_PlayerLifeIsDefined() {
 
+    @Test
+    void When_levelOfGuerrierIs10_Then_PlayerLifeIs50() {
+        Player player = new Player();
+        Personnages guerrier = new Guerrier();
+        guerrier.setLevel(10);
+        PersonnagesMaker makeGuerrier = new PersonnagesMaker();
+        makeGuerrier.definePlayerLife(player,guerrier);
+        assertEquals(50,player.getLifePlayer());
 
     }
-
     @Test
-    void levelTest() {
+    void When_TotalAbilitiesIsMoreHighOfLevelOfCharacter_Then_DisplayAbilityTooMuchExceptions() {
+        PersonnagesMaker makeGuerrier = new PersonnagesMaker();
+        makeGuerrier.nbLevel = 50;
+        assertThrows(AbilityTooMuchExceptions.class, () -> makeGuerrier.levelTest(51));
+
+
+
     }
 }
